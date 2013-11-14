0879fbdaead235161f07f87d4f7d50300878337b
iwsk <iwqbb963@kitc.city.kyoto.lg.jp>
2013-11-14 15:06:22 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HogeRails::Application.config.secret_key_base = '3f02a697e9784bf177eb482cc1adcdc334a277e12855e4f3f8ab024e5d492407bd945bffc3fd0f0e01ef1ed0674301cdc030b9336891b6538faf06031bdd2997'
